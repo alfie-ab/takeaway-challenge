b0dbff1244891c6ae37352965273111ade2cb88d
Alfie <alfie_ab@hotmail.co.uk>
2016-08-31 08:55:28 +0100

    doubles added for sms spec

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -2,6 +2,10 @@ require 'sms'
 
 describe Text do
 
+let(:text_class_double) { double :twilio_class_double, new: twilio}
+subject {described_class.new(twilio_class_double)}
+let(:twilio) { double :twilio, :messages => messages }
+lets(:messages) { double :messages, create: nil }
 
 
 end
